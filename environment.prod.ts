9cb478d86c01dc8ea3bcffa1606adb6d8cf56a0b
AbdulRaheem1308 <raheemc1308@gmail.com>
2026-05-06 14:26:23 +0530

    chore: update production URL and finalize README

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://enterprise-dashboard-api-2g74.onrender.com'
+  apiBaseUrl: 'https://enterprise-dashboard-api-2g74.onrender.com/api'
 };
